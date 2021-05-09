728c5f56953078ee2b69d63b180a83e462e075b0
umunnah <39714818+umunnah@users.noreply.github.com>
2021-05-09 10:13:56 +0100

    added db connection for production

--- knexfile.js
+++ knexfile.js
@@ -32,6 +32,28 @@ const connections = {
 			filename: "./mydb.sqlite",
 		},
 	},
+	production: {
+		client: process.env.DB_CLIENT,
+		connection: {
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			database: process.env.DB_NAME,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "sys_migrations",
+			directory: path.resolve(path.dirname(""), "src/database/migrations"),
+		},
+		seeds: {
+			directory: path.resolve(path.dirname(""), "src/database/seeds"),
+		},
+		acquireConnectionTimeout: 10000, // to timeout in 10
+	},
 };
 
 
